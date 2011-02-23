a9642c5f25b611fe50d59baaa92dd292ff291b93
Dan Quinlivan <darknrgy@gmail.com>
2011-02-23 01:55:57 -0800

    added menu bar to client, added connection dialog, started socket support (incomplete)

--- python/client.py
+++ python/client.py
@@ -1,4 +1,5 @@
 import wx
+import socketlib
 
 OUTPUTFIELD_SIZE = 75
 
@@ -162,10 +163,86 @@ class ThrottleControl:
         if self.throttle['right'] < -100: self.throttle['right'] = -100
         
         
-            
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(720,430))
+        self.statusbar = self.CreateStatusBar() # A StatusBar in the bottom of the window
+        self.statusbar.PushStatusText("Disconnected... use File -> Connect to connect to the robot")
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+        helpmenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuAbout = helpmenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        menuConnect = filemenu.Append(wx.ID_ANY, "&Connect..."," Open connection to robot")
+        menuDisconnect = filemenu.Append(wx.ID_ANY, "&Disconnect"," Disconnect from the robot")
+        menuExit = filemenu.Append(wx.ID_EXIT,"&Exit"," Terminate the program")        
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        menuBar.Append(helpmenu,"&Help") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnConnect, menuConnect)
+
+        self.Show(True)
+
+    def OnAbout(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, 
+"""Robot Client Beta
+Dan Quinlivan
+
+Begin by connecting to the robot with File -> connect
+
+w - accelerate
+s - decelerate
+a - turn left
+d - turn right
+
+""", "About Robot Client", wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
+
+    def OnExit(self,e):
+        self.Close(True)  # Close the frame.
         
+    def OnConnect(self,e):
+        """ Connect to the robot """
+        dlg = wx.TextEntryDialog(
+                self, 'Enter <server>:<port> to connect',
+                'Open connection to robot', "192.168.0.111:5333")
+
+        if dlg.ShowModal() == wx.ID_OK:
+            self.log.WriteText('You entered: %s\n' % dlg.GetValue())
+
+        dlg.Destroy()
+            
+
+class ProxyConnection(socketlib.Socket):
+    def __init__(self, host, port):
+        socketlib.Socket.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( (host, port) )
+        self.open = True
+        
+    def write(self, data):
+        socketlib.Socket.write(self, data)
+        
+                
+    def handle_request(self, req):
+        if self.user_sock.is_open():
+            self.user_sock.write(translated)
+
+
+            
 app = wx.App(False)
-frame = wx.Frame(None, size=(720,400))
+frame = MainWindow(None, "6WD Robot Client")
 panel = RobotClient(frame, 1)
 frame.Show()
 app.MainLoop()        
